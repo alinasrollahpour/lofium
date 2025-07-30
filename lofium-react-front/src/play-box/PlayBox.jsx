abbb430daa1bc960a281fb83751d4f7c9e09c417
ali <alinalamdari@gmail.com>
2025-07-30 08:53:27 +0330

    prototype the progress bar

--- lofium-react-front/src/play-box/PlayBox.jsx
+++ lofium-react-front/src/play-box/PlayBox.jsx
@@ -1,14 +1,16 @@
 import './PlayBox.css';
+import Progress from './Progress.jsx';
 import Control from './Control.jsx'
 
 function PlayBox() {
   return <div id='play-box'>
     <div id='top'>
-      <label id='seek'>1:37/3:52</label>
+      <label id='current-time'>1:37/3:52</label>
       <label id='title'>The ancient forest</label>
       <Control/>
     </div>
-    <div id='progress'></div>
+    <Progress/>
   </div>
 }
+
 export default PlayBox;
